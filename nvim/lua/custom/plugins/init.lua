e45c60d1f3e5f7bc6f58bb126c9aaf239989dc0b
samuka7abr <samuelabrao2006@gmail.com>
2025-10-27 09:06:52 -0300

    adicionando plugin no nvim

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -1,5 +1,34 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+-- Plugins customizados
+-- GitHub Copilot e CopilotChat configurados
+
+return {
+  -- GitHub Copilot (plugin oficial)
+  {
+    'github/copilot.vim',
+    event = 'InsertEnter',
+  },
+
+  -- Copilot Chat - Conversa com Copilot dentro do Neovim
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'github/copilot.vim' },
+      { 'nvim-lua/plenary.nvim' },
+    },
+    build = 'make tiktoken',
+    opts = {
+      debug = false,
+    },
+    keys = {
+      { '<leader>zc', ':CopilotChat<CR>', mode = 'n', desc = 'Chat com Copilot' },
+      { '<leader>ze', ':CopilotChatExplain<CR>', mode = 'v', desc = 'Explicar código selecionado' },
+      { '<leader>zr', ':CopilotChatReview<CR>', mode = 'v', desc = 'Revisar código' },
+      { '<leader>zf', ':CopilotChatFix<CR>', mode = 'v', desc = 'Corrigir bugs' },
+      { '<leader>zo', ':CopilotChatOptimize<CR>', mode = 'v', desc = 'Otimizar código' },
+      { '<leader>zd', ':CopilotChatDocs<CR>', mode = 'v', desc = 'Gerar documentação' },
+      { '<leader>zt', ':CopilotChatTests<CR>', mode = 'v', desc = 'Gerar testes' },
+      { '<leader>zm', ':CopilotChatCommit<CR>', mode = 'n', desc = 'Gerar mensagem de commit' },
+    },
+  },
+}
